47a601e378b3c5ce550d49560cd9a2cd111ab1b1
otake <otake.hiroyo@gmail.com>
2017-03-22 14:08:03 +0900

    MySQLの起動

---
+++ commands.sql
@@ -0,0 +1,16 @@
+-- MySQLの起動
+mysql -u root -p //password:root
+
+-- データベースの作成
+-- create database データベース名
+create database test;
+
+-- データベースの一覧表示
+show databases;
+
+-- データベースの削除
+-- drop databae データベース名
+drop database test;
+
+-- MySQLの終了
+exit;
