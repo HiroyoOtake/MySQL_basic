400277e95bcb35033f2a5ccbe52b6def1a35b9ba
otake <otake.hiroyo@gmail.com>
2017-03-24 13:55:40 +0900

    テーブルの作成

--- commands.sql
+++ commands.sql
@@ -14,3 +14,35 @@ drop database test;
 
 -- MySQLの終了
 exit;
+
+
+-- データベースの作成
+create database member;
+
+-- データベースの一覧表示
+show databases;
+
+-- 使用するデータベースを選ぶ
+use member;
+
+-- テーブルを見る
+show tables;
+
+-- テーブル作成
+create table memberList (id int, name varchar(32), pass char(4));
+
+-- テーブル構造を確認
+desc memberList;
+
+-- テストデータを挿入
+insert into memberList (id, name, pass) values (1, 'user1', '1111');
+insert into memberList (id, name, pass) values (2, 'user2', '2222');
+insert into memberList (id, name, pass) values (3, 'user3', '3333');
+
+-- テーブルのデータを表示
+select * from memberList;
+
+-- テーブルの削除
+drop table memberList;
+
+
